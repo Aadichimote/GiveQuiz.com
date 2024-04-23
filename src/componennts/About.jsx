1f4bc8d04e9e971c83bcb2b37fe3cbc013bf2bb1
Adi <adityachimote@gmail.com>
2024-04-23 12:43:41 +0530

    "final touch"

--- src/componennts/About.jsx
+++ src/componennts/About.jsx
@@ -8,10 +8,10 @@ export default function About() {
 
 
     <div className='source  md:w-[70%] w-full border  ml-20 mt-20' >
-        <p className=' source flex justify-center align-center ml-10 m-10  '>User-Friendly Design: The quiz interface is intuitively designed with a clean layout, making it easy for users to navigate and interact with quizzes.</p><p className=' source flex justify-center align-center ml-10 mt-10 '>Countdown Timer: A countdown timer feature adds an element of urgency and challenge to quizzes, motivating users to answer questions within a specified time limit.</p>
-        <p className=' source flex justify-center align-center ml-10 mt-10 '>Immediate Feedback: Users receive immediate feedback on their quiz performance, including the number of correct answers, incorrect answers, and overall score.</p>
-        <p className=' source flexjustify-center align-center ml-10 mt-10 '>Score Breakdown: The quiz results display a score breakdown, highlighting strengths and areas for improvement, fostering a learning-oriented approach.</p>
-        <p className=' bcot source flex justify-center align-center ml-10 mt-10 '> Quiz Focus: The quiz is centered around essential concepts and practices in HTML, CSS, and JavaScript (JS), covering a range of topics from basic syntax to advanced techniques.</p>
+        <p className=' source flex justify-center align-center ml-10 m-10  '><b>User-Friendly Design:</b> The quiz interface is intuitively designed with a clean layout, making it easy for users to navigate and interact with quizzes.</p><p className=' source flex justify-center align-center ml-10 mt-10 '><b className='p-1'>Countdown  Timer:</b>   A countdown timer feature adds an element of urgency and challenge to quizzes, motivating users to answer questions within a specified time limit.</p>
+        <p className=' source flex justify-center align-center ml-10 mt-10 '><b>Immediate Feedback:</b> Users receive immediate feedback on their quiz performance, including the number of correct answers, incorrect answers, and overall score.</p>
+        <p className=' source flexjustify-center align-center ml-10 mt-10 '><b>Score Breakdown:</b> The quiz results display a score breakdown, highlighting strengths and areas for improvement, fostering a learning-oriented approach.</p>
+        <p className=' bcot source flex justify-center align-center ml-10 mt-10 '> <b>Quiz Focus:</b> The quiz is centered around essential concepts and practices in HTML, CSS, and JavaScript (JS), covering a range of topics from basic syntax to advanced techniques.</p>
        
         
         
